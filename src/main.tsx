eeb8e3a416b978348710f554d3b106d6acf928b7
louremipsum <myself.vinayak2@gmail.com>
2024-01-07 23:07:02 +0530

    feat: mantine notification css imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
