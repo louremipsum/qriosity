faf46ae470489e48ed2169440073d892afbe23b1
louremipsum <myself.vinayak2@gmail.com>
2024-01-09 22:04:51 +0530

    feat: * React-router integration * Protected route functionality * Auth0 integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import "@mantine/notifications/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { Auth0Provider } from "@auth0/auth0-react";
+import Index from "./routes/Index.tsx";
+import ErrorPage from "./routes/ErrorPage.tsx";
+import PrivateRoute from "./routes/PrivateRoute.tsx";
+import ViewQR from "./routes/ViewQR.tsx";
 
 const theme = createTheme({
   colors: {
@@ -25,10 +30,36 @@ const theme = createTheme({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/app",
+    element: <PrivateRoute />,
+    children: [
+      { path: "/app/createqr", element: <App /> },
+      { path: "/app/viewqr", element: <ViewQR /> },
+      // other protected routes go here
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <App />
-    </MantineProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/app/createqr`,
+      }}
+    >
+      <MantineProvider theme={theme}>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
