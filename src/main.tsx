54868d7c3c9546a3955881fed8e6911ceb986368
louremipsum <myself.vinayak2@gmail.com>
2024-01-12 18:20:15 +0530

    feat: set routes/comp. for profile and viewQR page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MantineProvider, createTheme } from "@mantine/core";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -11,6 +11,8 @@ import Index from "./routes/Index.tsx";
 import ErrorPage from "./routes/ErrorPage.tsx";
 import PrivateRoute from "./routes/PrivateRoute.tsx";
 import ViewQR from "./routes/ViewQR.tsx";
+import CreateQR from "./routes/CreateQR.tsx";
+import Profile from "./routes/Profile.tsx";
 
 const theme = createTheme({
   colors: {
@@ -40,8 +42,9 @@ const router = createBrowserRouter([
     path: "/app",
     element: <PrivateRoute />,
     children: [
-      { path: "/app/createqr", element: <App /> },
+      { path: "/app/createqr", element: <CreateQR /> },
       { path: "/app/viewqr", element: <ViewQR /> },
+      { path: "/app/profile", element: <Profile /> },
       // other protected routes go here
     ],
     errorElement: <ErrorPage />,
