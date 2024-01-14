abe7f62b16ee008a09fc85477c229c31da85faf4
louremipsum <myself.vinayak2@gmail.com>
2024-01-14 17:06:46 +0530

    fix: error was previously of unknown type now gave it a type

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,16 +1,33 @@
 import { useRouteError } from "react-router-dom";
 
+type RouteError = {
+  statusText?: string;
+  message?: string;
+};
+
+const isError = (arg: unknown): arg is RouteError => {
+  return (
+    typeof arg === "object" &&
+    arg !== null &&
+    ("statusText" in arg || "message" in arg)
+  );
+};
+
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+  if (isError(error)) {
+    console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    );
+  }
+
+  return null;
 }
