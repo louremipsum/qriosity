5621300a1eb916d790ffb51f57a9285ede71df9a
louremipsum <myself.vinayak2@gmail.com>
2024-01-16 22:21:47 +0530

    feat: React Context API integrated fix: dirty fields are successfully identified and request is done successfully refactor: the main function was very complex so had to refactor

--- src/components/QRDetailCard.tsx
+++ src/components/QRDetailCard.tsx
@@ -24,6 +24,8 @@ import QRCodeComponent from "./QRCodeGen";
 import dayjs from "dayjs";
 import axios from "axios";
 import { notifications } from "@mantine/notifications";
+import { QRContext } from "../components/QRContext";
+import { useContext } from "react";
 
 type QRDetail = {
   qrObject?: QRCode;
@@ -41,7 +43,8 @@ type QRDetail = {
 const handleSubmit = async (
   event: React.FormEvent<HTMLFormElement>,
   form: UseFormReturnType<QRDetail>,
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  refreshQRs: () => void
 ) => {
   event.preventDefault();
 
@@ -49,18 +52,22 @@ const handleSubmit = async (
   const currentValues = form.values;
 
   // Determine which fields have been modified
-  const dirtyFields = Object.keys(currentValues).reduce<Partial<QRDetail>>(
-    (acc, key) => {
-      const fieldKey = key as keyof QRDetail;
-      if (form.isDirty(fieldKey)) {
-        // acc[fieldKey] = currentValues[fieldKey];
-        acc[fieldKey] = currentValues[fieldKey];
-      }
-      return acc;
-    },
-    {} as Partial<QRDetail>
-  );
+  const dirtyFields: Partial<QRDetail> = {};
 
+  if (form.isDirty("qrObject")) dirtyFields.qrObject = currentValues.qrObject;
+  if (form.isDirty("neverExpires"))
+    dirtyFields.neverExpires = currentValues.neverExpires;
+  if (form.isDirty("user")) dirtyFields.user = currentValues.user;
+  if (form.isDirty("scansLeft"))
+    dirtyFields.scansLeft = currentValues.scansLeft;
+  if (form.isDirty("expiry")) dirtyFields.expiry = currentValues.expiry;
+  if (form.isDirty("link")) dirtyFields.link = currentValues.link;
+  if (form.isDirty("id")) dirtyFields.id = currentValues.id;
+  if (form.isDirty("name")) dirtyFields.name = currentValues.name;
+  if (form.isDirty("desc")) dirtyFields.desc = currentValues.desc;
+  if (form.isDirty("infiniteScans"))
+    dirtyFields.infiniteScans = currentValues.infiniteScans;
+  dirtyFields.id = currentValues.id;
   // If any fields have been modified, send them to the API
   if (Object.keys(dirtyFields).length > 0) {
     try {
@@ -74,12 +81,14 @@ const handleSubmit = async (
           },
         }
       );
-      if (res.status !== 200) throw new Error("Error while saving QR Code");
+      if (res.data.statusCode !== 200)
+        throw new Error("Error while saving QR Code");
       notifications.show({
         color: "teal",
         title: "Success",
         message: "QR Code saved successfully!",
       });
+      refreshQRs();
     } catch (error) {
       notifications.show({
         color: "red",
@@ -92,9 +101,110 @@ const handleSubmit = async (
   }
 };
 
-const QRDetailCard = (props: QRDetail) => {
+const QRForm = ({
+  form,
+  setLoading,
+  loading,
+  refreshQRs,
+}: {
+  form: UseFormReturnType<QRDetail>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  refreshQRs: () => void;
+  loading: boolean;
+}) => {
   const dateIcon = <IconCalendar style={{ width: rem(16), height: rem(16) }} />;
+  return (
+    <form onSubmit={(e) => handleSubmit(e, form, setLoading, refreshQRs)}>
+      <SimpleGrid
+        cols={{ base: 1, sm: 2 }}
+        spacing="xl"
+        verticalSpacing="sm"
+        mt={"xl"}
+      >
+        <TextInput
+          withAsterisk
+          label="Name"
+          placeholder="Qriosity..."
+          {...form.getInputProps("name")}
+        />
+        <TextInput
+          label="Description"
+          placeholder="What is the QR for?"
+          {...form.getInputProps("desc")}
+        />
+        <NumberInput
+          withAsterisk
+          label="Valid Scans"
+          min={1}
+          placeholder="How many times can this QR be scanned?"
+          {...form.getInputProps("scansLeft")}
+        />
+        <Checkbox
+          label="Infinite Scans"
+          description="The QR can be scanned for any number of times"
+          mt={"xl"}
+          color="teal"
+          {...form.getInputProps("infiniteScans")}
+        />
+        <DateInput
+          withAsterisk
+          label="Expiry Date"
+          placeholder="When should the QR expire?"
+          rightSection={dateIcon}
+          color="teal"
+          disabled={form.values.neverExpires}
+          minDate={dayjs(new Date()).add(1, "day").toDate()}
+          {...form.getInputProps("expiry")}
+        />
+        <Checkbox
+          label="No Expiry?"
+          description="The QR will not have an expiry date"
+          mt={"xl"}
+          color="teal"
+          checked={form.values.neverExpires}
+          {...form.getInputProps("neverExpires")}
+        />
+        <TextInput
+          withAsterisk
+          label="Link"
+          placeholder="What link should be converted to QR Code?"
+          {...form.getInputProps("link")}
+        />
+      </SimpleGrid>
+      <Group justify="space-between">
+        <Button
+          radius="md"
+          color="teal"
+          mt="xl"
+          w={"40%"}
+          size="md"
+          variant="filled"
+          type="submit"
+          disabled={!form.isDirty()}
+          loading={loading}
+          leftSection={<IconDeviceFloppy />}
+        >
+          Save
+        </Button>
+        <Button
+          radius="md"
+          mt="md"
+          color="red"
+          size="md"
+          w={"40%"}
+          variant="outline"
+          leftSection={<IconTrash />}
+        >
+          Delete
+        </Button>
+      </Group>
+    </form>
+  );
+};
+
+const QRDetailCard = (props: QRDetail) => {
   const [loading, setLoading] = useState<boolean>(false);
+  const { refreshQRs } = useContext(QRContext);
   const form = useForm<QRDetail>({
     initialValues: {
       id: props.id,
@@ -130,91 +240,12 @@ const QRDetailCard = (props: QRDetail) => {
       <Text ta="center" fz="lg" fw={500} mt="sm">
         {props.name}
       </Text>
-      <form onSubmit={(e) => handleSubmit(e, form, setLoading)}>
-        <SimpleGrid
-          cols={{ base: 1, sm: 2 }}
-          spacing="xl"
-          verticalSpacing="sm"
-          mt={"xl"}
-        >
-          <TextInput
-            withAsterisk
-            label="Name"
-            placeholder="Qriosity..."
-            {...form.getInputProps("name")}
-          />
-          <TextInput
-            label="Description"
-            placeholder="What is the QR for?"
-            {...form.getInputProps("desc")}
-          />
-          <NumberInput
-            withAsterisk
-            label="Valid Scans"
-            min={1}
-            placeholder="How many times can this QR be scanned?"
-            {...form.getInputProps("scansLeft")}
-          />
-          <Checkbox
-            label="Infinite Scans"
-            description="The QR can be scanned for any number of times"
-            mt={"xl"}
-            color="teal"
-            {...form.getInputProps("infiniteScans")}
-          />
-          <DateInput
-            withAsterisk
-            label="Expiry Date"
-            placeholder="When should the QR expire?"
-            rightSection={dateIcon}
-            color="teal"
-            disabled={form.values.neverExpires}
-            minDate={dayjs(new Date()).add(1, "day").toDate()}
-            {...form.getInputProps("expiry")}
-          />
-          <Checkbox
-            label="No Expiry?"
-            description="The QR will not have an expiry date"
-            mt={"xl"}
-            color="teal"
-            checked={form.values.neverExpires}
-            {...form.getInputProps("neverExpires")}
-          />
-          <TextInput
-            withAsterisk
-            label="Link"
-            placeholder="What link should be converted to QR Code?"
-            {...form.getInputProps("link")}
-          />
-        </SimpleGrid>
-        <Group justify="space-between">
-          <Button
-            radius="md"
-            color="teal"
-            mt="xl"
-            w={"40%"}
-            size="md"
-            variant="filled"
-            type="submit"
-            disabled={!form.isDirty()}
-            loading={loading}
-            leftSection={<IconDeviceFloppy />}
-          >
-            Save
-          </Button>
-          <Button
-            radius="md"
-            mt="md"
-            color="red"
-            size="md"
-            w={"40%"}
-            variant="outline"
-            leftSection={<IconTrash />}
-          >
-            Delete
-          </Button>
-        </Group>
-      </form>
+      <QRForm
+        form={form}
+        setLoading={setLoading}
+        loading={loading}
+        refreshQRs={refreshQRs}
+      />
     </Card>
   );
 };
