5f5768a4bf5b6c03d39011c49b5cbe7369dc9f4d
louremipsum <myself.vinayak2@gmail.com>
2024-01-17 16:33:48 +0530

    refactor: statusCode enum for different situation and custom error interface

--- src/routes/ViewId.tsx
+++ src/routes/ViewId.tsx
@@ -4,17 +4,25 @@ import axios, { AxiosResponse } from "axios";
 import { validate as uuidValidate, version as uuidVersion } from "uuid";
 import ViewLink from "../components/ViewLink";
 
-type ResponseData = {
-  status?: string;
+enum StatusCode {
+  Ok = "200",
+  NotFound = "404",
+  BadRequest = "400",
+  Forbidden = "403",
+  InternalServerError = "500",
+}
+
+interface ResponseData {
+  status?: StatusCode;
   link?: string;
   message?: string;
-};
+}
 
-/**
- * Checks if the provided ID is a valid UUID and a particular version.
- * @param id The ID to be validated.
- * @returns True if the ID is a valid UUID mentioned version, false otherwise.
- */
+interface customError {
+  message: string;
+  status: StatusCode;
+}
+
 const isValidUUID = (id: string): boolean => {
   return (
     uuidValidate(id) &&
@@ -22,59 +30,35 @@ const isValidUUID = (id: string): boolean => {
   );
 };
 
-/**
- * Fetches data from the backend API based on the provided ID.
- * @param id - The ID of the data to fetch.
- * @returns A promise that resolves to the Axios response containing the fetched data.
- */
 const fetchUrl = async (id: string): Promise<AxiosResponse<ResponseData>> => {
   return axios.get(`${import.meta.env.VITE_BACKEND_VIEW_LINK}/${id}`);
 };
 
-/**
- * Checks if the provided ID is a valid UUID.
- * @param id - The ID to be checked.
- * @throws {Error} - Throws an error if the ID is not a valid UUID.
- */
 const checkUUID = (id: string) => {
-  if (!isValidUUID(id!)) {
+  if (!isValidUUID(id)) {
     throw new Error("Please Scan the QR again. Something unexpected happened");
   }
 };
 
-/**
- * Handles the response for a given ID.
- * @param id - The ID to fetch the response for.
- * @returns The response object.
- * @throws {Error} If an unexpected error occurs or if the link has expired.
- */
 const handleResponse = async (id: string) => {
-  const response = await fetchUrl(id!);
-
-  if (response.data.status !== "200") {
+  const response = await fetchUrl(id);
+  if (response.data.status !== StatusCode.Ok) {
     let message = "An unexpected error has occurred";
-    if (response.data.status === "400") {
+    if (response.data.status === StatusCode.BadRequest) {
       message = "We are sorry but the link has expired";
-    } else if (response.data.status === "500") {
+    } else if (response.data.status === StatusCode.Forbidden) {
+      message = "The link is yet to be activated";
+    } else if (response.data.status === StatusCode.InternalServerError) {
       message = "Error processing request";
     }
-    throw new Error(message);
+    throw { message, status: response.data.status };
   }
 
   return response;
 };
 
-/**
- * Handles the data received from the API response.
- * If the response does not contain a link, an error is thrown.
- * Otherwise, the link is returned.
- *
- * @param response - The Axios response containing the data.
- * @returns The link from the response data.
- * @throws Error if the link is not found in the response data.
- */
 const handleData = (response: AxiosResponse<ResponseData>) => {
-  if (response.data.status === "404") {
+  if (response.data.status === StatusCode.NotFound) {
     throw new Error("We are sorry but the link was not found");
   } else {
     return response.data.link;
@@ -84,28 +68,27 @@ const handleData = (response: AxiosResponse<ResponseData>) => {
 const ViewId = () => {
   const { id } = useParams();
   const [url, setUrl] = useState<string | null>(null);
-  const [error, setError] = useState<{ msg: string; status: number } | null>(
-    null
-  );
+  const [error, setError] = useState<customError | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const directHomePage = () => navigate("/");
 
   useEffect(() => {
-    /**
-     * Fetches data based on the provided ID.
-     * @returns {Promise<void>} A promise that resolves when the data is fetched.
-     */
     const fetchData = async (): Promise<void> => {
       setLoading(true);
       try {
-        checkUUID(id!);
-        const response = await handleResponse(id!);
-        const link = handleData(response);
-        setUrl(link!);
+        if (id) {
+          checkUUID(id);
+          const response = await handleResponse(id);
+          const link = handleData(response);
+          setUrl(link || null);
+        }
       } catch (err) {
-        setError({ msg: (err as Error).message, status: 500 });
+        setError({
+          message: (err as customError).message,
+          status: (err as customError).status,
+        });
       } finally {
         setLoading(false);
       }
@@ -114,9 +97,6 @@ const ViewId = () => {
     fetchData();
   }, [id]);
 
-  /**
-   * Handles the redirect to the link obtained from QR.
-   */
   const handleRedirect = () => {
     if (url) {
       const newWindow = window.open(url, "_blank");
@@ -134,8 +114,8 @@ const ViewId = () => {
   if (error) {
     return (
       <ViewLink
-        status={error.status.toString()}
-        title={error.msg}
+        status={error.status}
+        title={error.message}
         description="Please contact the QR administator for more information!"
         buttonText="Go to Home"
         buttonAction={directHomePage}
@@ -150,7 +130,7 @@ const ViewId = () => {
   const domain = new URL(url).hostname;
   return (
     <ViewLink
-      status="200"
+      status={StatusCode.Ok}
       title={`You are being redirected to ${domain}`}
       buttonText="Proceed"
       buttonAction={handleRedirect}
