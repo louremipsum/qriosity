db49c6de146ad2bc77c17d5dea21484611b3cefc
louremipsum <myself.vinayak2@gmail.com>
2024-01-22 21:57:35 +0530

    fix

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,10 +1,4 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from "@auth0/nextjs-auth0";
 
-export const GET = handleAuth({
-  async login(req, res) {
-    await handleLogin(req, res, {
-      returnTo: "/dashboard/createqr",
-    });
-  },
-});
+export const GET = handleAuth();
