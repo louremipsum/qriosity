07ec98f870aa583045d52e9dd9bb302aa55a8836
louremipsum <myself.vinayak2@gmail.com>
2024-01-25 08:40:53 +0530

    feat: create pages

---
+++ src/app/support/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Header from "@/components/common/Header";
+import { useDisclosure } from "@mantine/hooks";
+
+const Support = () => {
+  const [opened, { toggle }] = useDisclosure();
+  return <Header opened={opened} toggle={toggle} burger={false} />;
+};
+
+export default Support;
---
+++ src/app/terms-of-service/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Header from "@/components/common/Header";
+import { useDisclosure } from "@mantine/hooks";
+
+const TOS = () => {
+  const [opened, { toggle }] = useDisclosure();
+  return <Header opened={opened} toggle={toggle} burger={false} />;
+};
+
+export default TOS;
