07ecf564302e6d2a41cb0e4c2c7405b9d7062c2c
louremipsum <myself.vinayak2@gmail.com>
2024-02-01 19:04:57 +0530

    fix: "useSearchParams() should be wrapped in a suspense boundary at page "/return" as nothing would be displayed when no content is fetched

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -1,94 +1,58 @@
 "use client";
-// TODO: Seperate this logic into server and client side so fetch request is not made on client side
-import { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import Header from "@/components/common/Header";
-import { useDisclosure } from "@mantine/hooks";
-import {
-  Button,
-  Container,
-  Loader,
-  Mark,
-  Stack,
-  Text,
-  Flex,
-} from "@mantine/core";
-import { IconCircleCheck } from "@tabler/icons-react";
-import Link from "next/link";
+import { redirect, useSearchParams } from "next/navigation";
 import InvalidSessionPage from "@/components/payment/InvalidSessionPage";
+import { Loader, Flex } from "@mantine/core";
+import { Suspense, useEffect } from "react";
+import ReSessionStatus from "@/components/payment/ReSessionStatus";
+import { useState } from "react";
 
-export default function Return() {
-  const [data, setData] = useState<{
-    status: string;
-    customer_email: string;
-  }>({ status: "", customer_email: "" });
-  const searchParams = useSearchParams();
-  const sessionId = searchParams.get("session_id");
-  const router = useRouter();
-  const [opened, { toggle }] = useDisclosure();
-
-  useEffect(() => {
-    fetch(`/api/checkout-session?session_id=${sessionId}`, {
-      method: "GET",
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Invalid session");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        setData({ status: "invalid", customer_email: "" });
-      });
-  }, [sessionId]);
-
-  if (data.status === "open") {
-    return router.push("/");
-  }
-
-  if (data.status === "complete") {
-    return (
-      <>
-        <Header opened={opened} toggle={toggle} burger />
-        <Container size="md" mt={20} mb={20}>
-          <Stack align="center" justify="center" h={"80vh"}>
-            <IconCircleCheck
-              width={128}
-              height={128}
-              stroke={1}
-              style={{ color: "#00c7b0" }}
-            />
-            <Text ta="center" size="xl" fw={500}>
-              Thanks for Subscribing
-            </Text>
-            <Text ta="center" size="md" c={"dimmed"}>
-              A payment to Qriosityx Premium Plan will appear on your statement.
-              A confirmation email will be sent to{" "}
-              <Mark color="teal">{data.customer_email}</Mark>
-            </Text>
-            <Text ta="center" size="sm" c="dimmed" mt={"xl"}>
-              Log out and Log back in to start using your new plan
-            </Text>
-            <Link href="/api/auth/logout">
-              <Button variant={"filled"} color="teal">
-                Logout
-              </Button>
-            </Link>
-          </Stack>
-        </Container>
-      </>
-    );
-  }
-  if (data.status === "invalid") {
-    return <InvalidSessionPage />;
-  }
-
+const fallbackLoader = () => {
   return (
     <Flex justify="center">
       <Loader size={50} color="white" type="bars" />
     </Flex>
   );
-}
+};
+
+const ReturnPage = () => {
+  const queryParams = useSearchParams();
+  const sessionId = queryParams.get("session_id");
+  const [data, setData] = useState<{ status: string; customer_email: string }>({
+    status: "",
+    customer_email: "",
+  });
+  useEffect(() => {
+    const ses = async () => {
+      const session = await fetch(
+        `/api/checkout-session?session_id=${sessionId}`,
+        {
+          method: "GET",
+        }
+      );
+      const sessionData = await session.json();
+      setData(sessionData);
+    };
+    ses();
+  }, [sessionId]);
+
+  if (data.status === "open") {
+    redirect("/");
+  }
+
+  if (data.status === "complete") {
+    return <ReSessionStatus customer_email={data.customer_email} />;
+  }
+  if (data.status === "invalid") {
+    return <InvalidSessionPage />;
+  }
+};
+
+const Return = () => {
+  return (
+    <Suspense fallback={fallbackLoader()}>
+      <ReturnPage />
+    </Suspense>
+  );
+};
+
+export default Return;
---
+++ src/components/payment/ReSessionStatus.tsx
@@ -0,0 +1,46 @@
+import Header from "@/components/common/Header";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
+import { Button, Container, Mark, Stack, Text } from "@mantine/core";
+import { IconCircleCheck } from "@tabler/icons-react";
+
+type sessionData = {
+  customer_email: string;
+};
+
+const ReSessionStatus = (props: sessionData) => {
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <>
+      <Header opened={opened} toggle={toggle} burger />
+      <Container size="md" mt={20} mb={20}>
+        <Stack align="center" justify="center" h={"80vh"}>
+          <IconCircleCheck
+            width={128}
+            height={128}
+            stroke={1}
+            style={{ color: "#00c7b0" }}
+          />
+          <Text ta="center" size="xl" fw={500}>
+            Thanks for Subscribing
+          </Text>
+          <Text ta="center" size="md" c={"dimmed"}>
+            A payment to Qriosityx Premium Plan will appear on your statement. A
+            confirmation email will be sent to{" "}
+            <Mark color="teal">{props.customer_email}</Mark>
+          </Text>
+          <Text ta="center" size="sm" c="dimmed" mt={"xl"}>
+            Log out and Log back in to start using your new plan
+          </Text>
+          <Link href="/api/auth/logout">
+            <Button variant={"filled"} color="teal">
+              Logout
+            </Button>
+          </Link>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default ReSessionStatus;
