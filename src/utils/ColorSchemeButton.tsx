5394b58ad45a42168c72688da38fc34237801016
louremipsum <myself.vinayak2@gmail.com>
2024-02-11 16:44:21 +0530

    feat: text prop to conditionally render a text color scheme button

--- src/utils/ColorSchemeButton.tsx
+++ src/utils/ColorSchemeButton.tsx
@@ -2,22 +2,42 @@ import {
   ActionIcon,
   useComputedColorScheme,
   useMantineColorScheme,
+  Text,
+  rem,
 } from "@mantine/core";
 import { IconSun, IconMoon } from "@tabler/icons-react";
 import cx from "clsx";
 import classes from "@/styles/index.module.css";
 
-const ColorSchemeButton = () => {
+type ColorSchemeButtonType = {
+  text?: boolean;
+};
+
+const ColorSchemeButton = ({ text }: ColorSchemeButtonType) => {
   const { setColorScheme } = useMantineColorScheme({ keepTransitions: true });
   const computedColorScheme = useComputedColorScheme("light", {
     getInitialValueInEffect: true,
   });
 
-  return (
+  const nextColorScheme = computedColorScheme === "light" ? "dark" : "light";
+
+  return text ? (
+    <div
+      onClick={() => setColorScheme(nextColorScheme)}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      {nextColorScheme === "dark" ? (
+        <IconMoon style={{ width: rem(24), height: rem(24) }} stroke={1.2} />
+      ) : (
+        <IconSun style={{ width: rem(24), height: rem(24) }} stroke={1.2} />
+      )}
+      <Text span fz={"md"} ml={"xs"}>
+        Switch to {nextColorScheme}
+      </Text>
+    </div>
+  ) : (
     <ActionIcon
-      onClick={() =>
-        setColorScheme(computedColorScheme === "light" ? "dark" : "light")
-      }
+      onClick={() => setColorScheme(nextColorScheme)}
       variant="default"
       size="xl"
       aria-label="Toggle color scheme"
