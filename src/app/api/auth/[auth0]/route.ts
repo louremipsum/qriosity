d2ec517c2d0f540e795df8fb130d57540bb7673f
louremipsum <myself.vinayak2@gmail.com>
2024-02-13 23:57:14 +0530

    feat: error handling for Auth0 authentication feat: error handling if user email is not verified

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,4 +1,25 @@
-// app/api/auth/[auth0]/route.js
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { NextRequest } from "next/server";
+import {
+  handleAuth,
+  AppRouterOnError,
+  HandlerError,
+} from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
-export const GET = handleAuth();
+const onError: AppRouterOnError = (req: NextRequest, error: HandlerError) => {
+  console.error(
+    "Error occurred in Auth0 handleAuth:\nError Name:",
+    error.name,
+    "\nError Message:",
+    error.message,
+    "\nError Code:",
+    error.code,
+    "\nRequest:",
+    req
+  );
+  redirect(`/error/notverify${req.nextUrl.search}`);
+};
+
+export const GET = handleAuth({
+  onError,
+});
