8c47aff43ebbe1a0e5f7fb96a99e08ee4145d7f1
louremipsum <myself.vinayak2@gmail.com>
2024-02-28 12:16:47 +0530

    fix: removed role variable from context and provider

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,14 +1,11 @@
-// src/context/QRContext.tsx
 "use client";
 import React from "react";
 
 export interface QRContextType {
-  numQRs: number;
-  setNumQRs: React.Dispatch<React.SetStateAction<number>>;
+  numQRs: number | undefined;
+  setNumQRs: React.Dispatch<React.SetStateAction<number | undefined>>;
   dataLoaded: boolean;
   setDataLoaded: React.Dispatch<React.SetStateAction<boolean>>;
-  role: string;
-  setRole: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const QRContext = React.createContext<QRContextType | undefined>(
--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -5,13 +5,12 @@ import { QRContext } from "./Context";
 export const QRProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [numQRs, setNumQRs] = React.useState(0);
-  const [dataLoaded, setDataLoaded] = React.useState(false);
-  const [role, setRole] = React.useState("");
+  const [numQRs, setNumQRs] = React.useState<number | undefined>(0);
+  const [dataLoaded, setDataLoaded] = React.useState<boolean>(false);
 
   return (
     <QRContext.Provider
-      value={{ numQRs, setNumQRs, dataLoaded, setDataLoaded, role, setRole }}
+      value={{ numQRs, setNumQRs, dataLoaded, setDataLoaded }}
     >
       {children}
     </QRContext.Provider>
